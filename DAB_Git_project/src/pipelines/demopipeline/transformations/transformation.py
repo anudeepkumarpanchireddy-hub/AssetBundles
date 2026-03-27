e933bf7dda5f85ba4e5b444372002b95baa8c49e
anudeepkumarpanchireddy-hub <anudeepkumarpanchireddy@gmail.com>
2026-03-27 18:55:01 +0000

    first commit

--- DAB_Git_project/src/pipelines/demopipeline/transformations/transformation.py
+++ DAB_Git_project/src/pipelines/demopipeline/transformations/transformation.py
@@ -1,4 +1,4 @@
-from pyspark.sql.functions import pipelines as dp
+from pyspark import pipelines as dp
 from pyspark.sql.types import *
 
 from pyspark.sql.functions import *
@@ -6,12 +6,9 @@ from pyspark.sql.functions import *
 
 @dp.table(name = "mycatalogdata.default.bronze_rider_vehicle_details")
 def bronze_rider_vehicle_details():
-  return (spark.readStream
-            .format("cloudFiles")
-            .option("cloudFiles.format", "json")
-            .option("cloudFiles.inferColumnTypes", "true")
-            .load(f"dev.default.rider_vehicle_details")
-            )
+  return (spark.read.table("dev.default.rider_vehicle_details"))
+
+  
 @dp.table(name = "mycatalogdata.default.silver_rider_vehicle_details")
 def read_the_Bronze_data():
     return (
@@ -21,6 +18,6 @@ def read_the_Bronze_data():
     
 @dp.materialized_view(name = "mycatalogdata.default.gold_rider_vehicle_details")
 def read_the_Silver_data():
-    return (dp.readStream("dev.default.rider_vehicle_details")
-        .filter(col("vehicle_type_id") == 2).count()
+    return (dp.read("dev.default.rider_vehicle_details")
+        .filter(col("vehicle_type_id") == 2)
            )
